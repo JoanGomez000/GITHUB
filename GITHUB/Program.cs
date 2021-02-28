b6a5d05ab56735bb79f657ec17fc008b44d4ce19
joan <joan.gomez2002@gmail.com>
2021-02-28 22:26:35 +0100

    Master

--- GITHUB/Program.cs
+++ GITHUB/Program.cs
@@ -10,6 +10,49 @@ namespace GITHUB
     {
         static void Main(string[] args)
         {
+            char continuar_programa = 'S';
+            do
+            {
+                Console.WriteLine("|**********************************|");
+                Console.WriteLine("|*                                *|");
+                Console.WriteLine("|*  1 - Exàmen de conduir:        *|");
+                Console.WriteLine("|*  2 - Exàmen de matemàtiques:   *|");
+                Console.WriteLine("|*  3 - Exàmen de programació:    *|");
+                Console.WriteLine("|*                                *|");
+                Console.WriteLine("|**********************************|");
+
+                char opcio_triar = char.Parse(Console.ReadLine());
+
+                switch (opcio_triar)
+                {
+                    case '1':
+                        {
+                            Console.Clear();
+                            PreguntaConduir1();
+                            Console.ReadKey();
+                        }
+                        break;
+                    case '2':
+                        {
+                            Console.Clear();
+                            PreguntaProgramacio();
+                            Console.ReadKey();
+                        }
+                        break;
+                    case '3':
+                        {
+                            Console.Clear();
+                            PreguntaBaseDeDades();
+                            Console.ReadKey();
+                        }
+                        break;
+                }
+
+                Console.Clear();
+                Console.WriteLine("\n|*  Vols continuar? S/N");
+                continuar_programa = char.Parse(Console.ReadLine());
+                Console.Clear();
+            } while (continuar_programa != 'N');
         }
     }
 }
