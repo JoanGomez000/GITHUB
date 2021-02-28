55199bd7dad1833b514ebdff52ba1ea7edc2df8a
joan <joan.gomez2002@gmail.com>
2021-02-28 22:29:47 +0100

    Branca 1 V1

--- GITHUB/Program.cs
+++ GITHUB/Program.cs
@@ -54,5 +54,116 @@ namespace GITHUB
                 Console.Clear();
             } while (continuar_programa != 'N');
         }
+        static void PreguntaConduir1()
+        {
+            Console.WriteLine("\n\n|*  La influencia del factor de riesgo vía, como se puede disminuir?\n\n");
+            Console.WriteLine("|*          A - Circulando de noche porque hay menos tráfico:");
+            Console.WriteLine("|*          B - Adecuando la velocidad y aumentando la distancia de seguridad:");
+            Console.WriteLine("|*          C - Circulando de noche porque hay menos tráfico:");
+            Console.WriteLine("|*          D - Sin hacer nada:");
+            Console.WriteLine("|* ");
+            Console.WriteLine("|********************************************************************************|");
+
+            Console.Write("|*   Tria una opció: ");
+            char ABCD = char.Parse(Console.ReadLine());
+
+            int acert = 0;
+            if (ABCD == 'B')
+            {
+                Console.WriteLine("|* ");
+                Console.WriteLine("|* ");
+                Console.WriteLine("|********************************************************************************|");
+                Console.WriteLine("|*                          PREGUNTA ACERTADA!                                    ");
+                Console.WriteLine("|********************************************************************************|");
+                acert++;
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("|* ");
+                Console.WriteLine("|* ");
+                Console.WriteLine("|********************************************************************************|");
+                Console.WriteLine("|*                          PREGUNTA INCORRECTE!                                    ");
+                Console.WriteLine("|********************************************************************************|");
+                Console.ReadKey();
+            }
+            Console.Clear();
+
+
+
+
+
+            Console.WriteLine("\n\n|*  En una calzada con los dos sentidos separados por una línea continua, esta permitido girar a la izquierda?\n\n");
+            Console.WriteLine("|*          A - Sí, cuando no se comprometa la seguridad de ningún usuario de la vía.");
+            Console.WriteLine("|*          B - No.");
+            Console.WriteLine("|*          C - Solo dentro de poblado");
+            Console.WriteLine("|*          D - Solo fuera de poblado");
+            Console.WriteLine("|* ");
+            Console.WriteLine("|********************************************************************************|");
+
+            Console.Write("|*   Tria una opció: ");
+            ABCD = char.Parse(Console.ReadLine());
+
+
+            if (ABCD == 'B')
+            {
+                Console.WriteLine("|* ");
+                Console.WriteLine("|* ");
+                Console.WriteLine("|********************************************************************************|");
+                Console.WriteLine("|*                          PREGUNTA ACERTADA!                                    ");
+                Console.WriteLine("|********************************************************************************|");
+                Console.ReadKey();
+                acert++;
+            }
+            else
+            {
+                Console.WriteLine("|* ");
+                Console.WriteLine("|* ");
+                Console.WriteLine("|********************************************************************************|");
+                Console.WriteLine("|*                          PREGUNTA INCORRECTE!                                    ");
+                Console.WriteLine("|********************************************************************************|");
+                Console.ReadKey();
+            }
+            Console.Clear();
+
+
+
+
+            Console.WriteLine("\n\n|*  Quien tiene la responsabilidad por las infracciones cometidas sobre las normas de circulacion?\n\n");
+            Console.WriteLine("|*          A - El conductor y si es menor de edad, sus padres o tutores legales.");
+            Console.WriteLine("|*          B - Siempre el conductor causante de la infracción cometida.");
+            Console.WriteLine("|*          C - El titular del vehículo");
+            Console.WriteLine("|*          D - Tus padres");
+            Console.WriteLine("|* ");
+            Console.WriteLine("|********************************************************************************|");
+
+            Console.Write("|*   Tria una opció: ");
+            ABCD = char.Parse(Console.ReadLine());
+
+
+            if (ABCD == 'A')
+            {
+                Console.WriteLine("|* ");
+                Console.WriteLine("|* ");
+                Console.WriteLine("|********************************************************************************|");
+                Console.WriteLine("|*                          PREGUNTA ACERTADA!                                    ");
+                Console.WriteLine("|********************************************************************************|");
+                acert++;
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("|* ");
+                Console.WriteLine("|* ");
+                Console.WriteLine("|********************************************************************************|");
+                Console.WriteLine("|*                          PREGUNTA INCORRECTE!                                    ");
+                Console.WriteLine("|********************************************************************************|");
+                Console.ReadKey();
+            }
+            Console.Clear();
+
+        }
+
     }
 }
+
